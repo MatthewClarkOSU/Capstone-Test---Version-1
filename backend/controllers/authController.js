e510b6c2c581dd86be574a71fcda3f336db65a5e
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-07-29 19:23:31 -0400

    added token functionality

---
+++ backend/controllers/authController.js
@@ -0,0 +1,52 @@
+const User = require('../models/user');
+
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncError = require('../middlewares/catchAsyncErrors');
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
+
+// Register a user -> /api/v1/register
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+
+    const { name, email, password } = req.body;
+
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: 'Bob',
+            url: 'https://tvline.com/wp-content/uploads/2020/11/bob-burgers-episode-200-video.jpg'
+        }
+    })
+
+    sendToken(user, 200, res)
+
+})
+
+// Login User -> /api/v1/login
+exports.loginUser = catchAsyncErrors( async(req, res, next) => {
+    const { email, password } = req.body;
+
+    // Check if email and password are entered by user
+    if(!email || !password) {
+        return next(new ErrorHandler('Please enter email and password', 400))
+    }
+
+    // Find user in the database
+    const user = await User.findOne({ email }).select('+password')
+
+    if(!user) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    // Check if the password is correct or not
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    sendToken(user, 200, res)
+})
