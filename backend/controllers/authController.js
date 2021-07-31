54b79588e54f5b0d81452486bba792752d42f94e
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-07-31 13:55:14 -0400

    added password recovery

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,12 @@
 const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
-const catchAsyncError = require('../middlewares/catchAsyncErrors');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const sendToken = require('../utils/jwtToken');
+const sendEmail = require('../utils/sendEmail');
+
+const crypto = require('crypto')
+
 
 // Register a user -> /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -66,8 +69,62 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
     await user.save({ validateBeforeSave: false })
 
     // Create reset password url
-    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/reset/${resetToken}`
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
 
+    const message = `Your password reset token is as follows: \n\n${resetUrl}\n\nIf you have not requested this email, then please ignore it.`
+
+    try {
+
+        await sendEmail({
+            email: user.email,
+            subject: 'Password Recovery',
+            message
+        })
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to: ${user.email}`
+        })
+        
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500))
+    }
+
+})
+
+// Reset Password -> /api/v1/password/reset/:token
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+
+    // Hash URL token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if(!user) {
+        return next(new ErrorHandler('Password reset token is invalid or has expired', 400)
+        )
+    }
+    if(req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match', 400))
+    }
+    
+    // Setup new password
+    user.password = req.body.password;
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res)
 })
 
 
