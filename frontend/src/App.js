d90c9fff2eb11c3b91cfe7e8e0c8f2fbb55d2067
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-08-05 16:24:36 -0400

    added update profile functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import ProductDetails from './components/product/productDetails'
 import Login from './components/user/Login'
 import Register from './components/user/Register'
 import Profile from './components/user/Profile'
+import UpdateProfile from './components/user/UpdateProfile'
 
 import ProtectedRoute from './components/route/ProtectedRoute'
 
@@ -33,6 +34,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <ProtectedRoute path="/me" component={Profile} exact />
+          <ProtectedRoute path="/me/update" component={UpdateProfile} exact />
 
         </div>
         <Footer />
