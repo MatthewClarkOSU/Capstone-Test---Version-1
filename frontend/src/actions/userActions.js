da5c14aecb1a1fec699cc21d7eb3799a302e54e9
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-08-10 15:05:33 -0400

    added delete user admin functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -33,6 +33,9 @@ import {
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
     USER_DETAILS_FAIL,
+    DELETE_USER_REQUEST,
+    DELETE_USER_SUCCESS,
+    DELETE_USER_FAIL,
     CLEAR_ERRORS
 } from '../constants/userConstants'
 
@@ -301,6 +304,27 @@ export const getUserDetails = (id) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: USER_DETAILS_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+// Delete user (Admin)
+export const deleteUser = (id) => async (dispatch) => {
+    try {
+
+        dispatch({ type: DELETE_USER_REQUEST })
+
+        const { data } = await axios.delete(`/api/v1/admin/user/${id}`)
+
+        dispatch({
+            type: DELETE_USER_SUCCESS,
+            payload: data.success
+        })
+
+    } catch (error) {
+        dispatch({
+            type: DELETE_USER_FAIL,
             payload: error.response.data.message
         })
     }
--- frontend/src/components/admin/UsersList.js
+++ frontend/src/components/admin/UsersList.js
@@ -8,15 +8,16 @@ import Sidebar from './Sidebar'
 
 import { useAlert } from 'react-alert'
 import { useDispatch, useSelector } from 'react-redux'
-import { allUsers, clearErrors } from '../../actions/userActions'
-// import { DELETE_USER_RESET } from '../../constants/userConstants'
+import { allUsers, deleteUser, clearErrors } from '../../actions/userActions'
+import { DELETE_USER_RESET } from '../../constants/userConstants'
 
-const UsersList = () => {
+const UsersList = ({ history }) => {
 
     const alert = useAlert();
     const dispatch = useDispatch();
 
     const { loading, error, users } = useSelector(state => state.allUsers);
+    const { isDeleted } = useSelector(state => state.user)
 
     useEffect(() => {
         dispatch(allUsers());
@@ -26,13 +27,17 @@ const UsersList = () => {
             dispatch(clearErrors())
         }
 
-        // if (isDeleted) {
-        //     alert.success('Product has been deleted successfully');
-        //     history.push('/admin/products');
-        //     dispatch({ type: DELETE_PRODUCT_RESET })
-        // }
+        if (isDeleted) {
+            alert.success('User has been deleted successfully');
+            history.push('/admin/users');
+            dispatch({ type: DELETE_USER_RESET })
+        }
 
-    }, [dispatch, alert, error])
+    }, [dispatch, alert, error, isDeleted, history])
+
+    const deleteUserHandler = (id) => {
+        dispatch(deleteUser(id))
+    }
 
     const setUsers = () => {
         const data = {
@@ -75,7 +80,7 @@ const UsersList = () => {
                     <Link to={`/admin/user/${user._id}`} className="btn btn-primary py-1 px-2">
                         <i className="fa fa-pencil"></i>
                     </Link>
-                    <button className="btn btn-danger py-1 px-2 ml-2">
+                    <button className="btn btn-danger py-1 px-2 ml-2" onClick={() => deleteUserHandler(user._id)}>
                         <i className="fa fa-trash"></i>
                     </button>
                 </Fragment>
